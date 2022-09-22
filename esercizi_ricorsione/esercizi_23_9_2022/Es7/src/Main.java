2ce67a0f9ab5305eb7dce7a1df7ed80a4e5718c5
SimVac <simonevacca932@gmail.com>
2022-09-22 23:54:47 +0200

    Aggiunge esercizio sette sulla ricorsione

---
+++ esercizi_ricorsione/esercizi_23_9_2022/Es7/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static int sommaDispari(int[] v, int n){
+        if (n < 1)
+            return 0;
+        return sommaDispari(v, n - 2) + v[v.length - n];
+    }
+
+    /**
+     * Funzione per inizializzare un vettore con numeri casuali compresi tra 0 e 9 inclusi
+     * @param v Vettore da inizializzare
+     * @param n Lunghezza del vettore
+     */
+    public static void inizializzaVettore(int[] v, int n){
+        for (int i = 0; i < n; i++) {
+            v[i] = (int)(Math.random() * 10);
+        }
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        int[] v = new int[n];
+        inizializzaVettore(v, n);
+        System.out.println("Vettore:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(v[i]);
+        }
+        System.out.println("Somma dei posti dispari del vettore: " + sommaDispari(v, n));
+    }
+}
