0b4aeb0ad0eb3e46dd1919c9cb58bf441d5b8ab4
SimVac <simonevacca932@gmail.com>
2022-09-22 23:35:15 +0200

    Aggiunge esercizio cinque sulla ricorsione

---
+++ esercizi_ricorsione/esercizi_23_9_2022/Es5/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static boolean ricerca(int[] v, int valore, int i, int f){
+        if (v[i] == valore)
+            return true;
+        if (i >= f)
+            return false;
+        return ricerca(v, valore, i + 1, (i + f) / 2) || ricerca(v, valore, (i + f) / 2 + 1, f);
+    }
+
+    /**
+     * Funzione per inizializzare un vettore con numeri casuali compresi tra 0 e 9 inclusi
+     * @param v Vettore da inizializzare
+     * @param n Lunghezza del vettore
+     */
+    public static void inizializzaVettore(int[] v, int n){
+        for (int i = 0; i < n; i++) {
+            v[i] = (int)(Math.random() * 10);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 100;
+        int[] v = new int[n];
+        inizializzaVettore(v, n);
+        System.out.println("Vettore:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(v[i]);
+        }
+        int valoreRicercato = 4;
+        if (ricerca(v, valoreRicercato, 0, n - 1))
+            System.out.println("Valore trovato");
+        else
+            System.out.println("Valore non trovato");
+    }
+}
